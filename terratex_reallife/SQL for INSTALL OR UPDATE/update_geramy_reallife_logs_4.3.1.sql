cb133feee22b09c9b5e1d23121d52cfc9412ad10
geramy <admin@terratex.eu>
2016-06-08 22:47:33 +0200

    add Steuer log table

---
+++ terratex_reallife/SQL for INSTALL OR UPDATE/update_geramy_reallife_logs_4.3.1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `steuer_log` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(250) NOT NULL
+  `Type` varchar(250) NOT NULL
+  `Betrag` int NOT NULL
+  PRIMARY KEY (ID)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;

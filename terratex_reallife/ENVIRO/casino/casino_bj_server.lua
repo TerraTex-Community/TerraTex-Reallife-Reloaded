f788e0ab75b178fa70e8534ec89cca86452d20bf
geramy <admin@terratex.eu>
2016-06-12 20:16:48 +0200

    Better Queries on Casino

--- terratex_reallife/ENVIRO/casino/casino_bj_server.lua
+++ terratex_reallife/ENVIRO/casino/casino_bj_server.lua
@@ -1,35 +1,45 @@
-local saveTagesGewinnePerSpieler={}--index = spieler , tabelle: {["pferde"]=gewinnpferde,["bj"]=gewinn}
+local saveTagesGewinnePerSpieler = {} --index = spieler , tabelle: {["pferde"]=gewinnpferde,["bj"]=gewinn}
 
-addEvent("reduceBlackJackHandMoney",true)
-function reduceBlackJackHandMoney_func(money,typ)
-    changePlayerMoney(source,-money,"sonstiges","Casino","Wetteinsatz")
+addEvent("reduceBlackJackHandMoney", true)
+function reduceBlackJackHandMoney_func(money, typ)
+    changePlayerMoney(source, -money, "sonstiges", "Casino", "Wetteinsatz")
     changeBizKasse(13, money, "Casino Wetteinsatz")
-	outputChatBox(string.format("Dein Einsatz wurde um %s erhöht!", toprice(money)),source,0,255,0)
-	--outputChatBox("Aufgrund von Bugusing wurde der Einsatz nur virtuell erhöht ohne vom Handgeld abgezogen zu werden!",source,255,0,0)
-	if not saveTagesGewinnePerSpieler[getPlayerName(source)] then saveTagesGewinnePerSpieler[getPlayerName(source)]={} end
-	if not saveTagesGewinnePerSpieler[getPlayerName(source)][typ] then saveTagesGewinnePerSpieler[getPlayerName(source)][typ]=0 end
-	saveTagesGewinnePerSpieler[getPlayerName(source)][typ]=saveTagesGewinnePerSpieler[getPlayerName(source)][typ]-money
-	local query="INSERT INTO log_casino (Nickname,Spiel,Betrag,tagesgewinn) VALUES ('"..getPlayerName(source).."','"..typ.."','"..-money.."','"..saveTagesGewinnePerSpieler[getPlayerName(source)][typ].."')"
-	mysql_query(logs_handler,query);
-	
+    outputChatBox(string.format("Dein Einsatz wurde um %s erhöht!", toprice(money)), source, 0, 255, 0)
+    --outputChatBox("Aufgrund von Bugusing wurde der Einsatz nur virtuell erhöht ohne vom Handgeld abgezogen zu werden!",source,255,0,0)
+    if not saveTagesGewinnePerSpieler[getPlayerName(source)] then saveTagesGewinnePerSpieler[getPlayerName(source)] = {} end
+    if not saveTagesGewinnePerSpieler[getPlayerName(source)][typ] then saveTagesGewinnePerSpieler[getPlayerName(source)][typ] = 0 end
+    saveTagesGewinnePerSpieler[getPlayerName(source)][typ] = saveTagesGewinnePerSpieler[getPlayerName(source)][typ] - money
+
+    MySql.helper.insert("log_casino", {
+        Nickname = getPlayerName(source),
+        Spiel = typ,
+        Betrag = -money,
+        tagesgewinn = saveTagesGewinnePerSpieler[getPlayerName(source)][typ]
+    });
 end
-addEventHandler("reduceBlackJackHandMoney",getRootElement(),reduceBlackJackHandMoney_func)
+
+addEventHandler("reduceBlackJackHandMoney", getRootElement(), reduceBlackJackHandMoney_func)
 
 
-addEvent("payOut_Bj",true)
-function payOut_Bj_func(money,typ)
-    changePlayerMoney(source,money,"sonstiges","Casino","Auszahlung")
+addEvent("payOut_Bj", true)
+function payOut_Bj_func(money, typ)
+    changePlayerMoney(source, money, "sonstiges", "Casino", "Auszahlung")
     changeBizKasse(13, money, "Casino Auszahlung")
-	outputChatBox(string.format("Dein Gewinn %s wurde ausgezahlt!", toprice(money)),source,0,255,0)
-	--outputChatBox("Aufgrund von Bugusing wurde der Gewinn nur virtuell ausgezahlt ohne auf das Handgeld ausgezahlt zu werden!",source,255,0,0)
-	if not saveTagesGewinnePerSpieler[getPlayerName(source)] then saveTagesGewinnePerSpieler[getPlayerName(source)]={} end
-	if not saveTagesGewinnePerSpieler[getPlayerName(source)][typ] then saveTagesGewinnePerSpieler[getPlayerName(source)][typ]=0 end
-	saveTagesGewinnePerSpieler[getPlayerName(source)][typ]=saveTagesGewinnePerSpieler[getPlayerName(source)][typ]+money
-	local query="INSERT INTO log_casino (Nickname,Spiel,Betrag,tagesgewinn) VALUES ('"..getPlayerName(source).."','"..typ.."','"..money.."','"..saveTagesGewinnePerSpieler[getPlayerName(source)][typ].."')"
-	mysql_query(logs_handler,query);	
-	
+    outputChatBox(string.format("Dein Gewinn %s wurde ausgezahlt!", toprice(money)), source, 0, 255, 0)
+    --outputChatBox("Aufgrund von Bugusing wurde der Gewinn nur virtuell ausgezahlt ohne auf das Handgeld ausgezahlt zu werden!",source,255,0,0)
+    if not saveTagesGewinnePerSpieler[getPlayerName(source)] then saveTagesGewinnePerSpieler[getPlayerName(source)] = {} end
+    if not saveTagesGewinnePerSpieler[getPlayerName(source)][typ] then saveTagesGewinnePerSpieler[getPlayerName(source)][typ] = 0 end
+    saveTagesGewinnePerSpieler[getPlayerName(source)][typ] = saveTagesGewinnePerSpieler[getPlayerName(source)][typ] + money
+
+    MySql.helper.insert("log_casino", {
+        Nickname = getPlayerName(source),
+        Spiel = typ,
+        Betrag = money,
+        tagesgewinn = saveTagesGewinnePerSpieler[getPlayerName(source)][typ]
+    });
 end
-addEventHandler("payOut_Bj",getRootElement(),payOut_Bj_func)
+
+addEventHandler("payOut_Bj", getRootElement(), payOut_Bj_func)
 
 
 

39b56f0fe2fc5d33c7a0e63e0f9b6ff51471ab2f
Colin Weber <colin.weber@sap.com>
2016-06-13 16:41:58 +0200

    Refactor mysql_querys in faq/server.lua

--- terratex_reallife/HELPSYSTEM/hilfeguis/faq/server.lua
+++ terratex_reallife/HELPSYSTEM/hilfeguis/faq/server.lua
@@ -1,58 +1,41 @@
-local faq={}
+local faq = {}
 
+function loadFAQ()
+    local query = "SELECT * FROM faq LEFT JOIN faq_kat ON faq_kat.ID = faq.katID ORDER BY faq_kat.Order";
+    local runQuery = dbQuery(MySql._connection, query);
+    local result = dbPoll(runQuery, -1);
 
-function loadFAQ ( )
-	local query="SELECT * FROM faq LEFT JOIN faq_kat ON faq_kat.ID = faq.katID ORDER BY faq_kat.Order"
-	
-	local result=mysql_query(handler,query)
-	if( not result) then
-		 outputDebugString("Error executing the query: (" .. mysql_errno(handler) .. ") " .. mysql_error(handler))
-	else	
-		local zahler=0		
-		local rules={}
-		local katCounter=0
-		local katIDs={}
-		
-		while (mysql_num_rows(result)>zahler) do
-			local dasatz = mysql_fetch_assoc(result)
-			-- Antwort Frage kat
-			local tmpTable={}
-			local tmpID=katCounter+1
-			if(katIDs[dasatz["kat"]])then
-				tmpID=katIDs[dasatz["kat"]]
-			else
-				katIDs[dasatz["kat"]]=tmpID		
-				faq[tmpID]={
-					["title"]=dasatz["kat"],
-					["rules"]={},	
-				}
-				katCounter=katCounter+1
-			end
-			
-			tmpTable={
-				["text"]=dasatz["Frage"],
-				["answer"]=dasatz["Antwort"]
-			
-			}
-			
-			table.insert(faq[tmpID]["rules"],tmpTable)
-			zahler=zahler+1
-		end
-	end
+        local rules = {}
+    local katCounter = 0
+    local katIDs = {}
+
+    for theKey, dasatz in ipairs(result) do
+        -- Antwort Frage kat
+        local tmpTable = {}
+        local tmpID = katCounter + 1
+        if (katIDs[dasatz["kat"]]) then
+            tmpID = katIDs[dasatz["kat"]]
+        else
+            katIDs[dasatz["kat"]] = tmpID
+            faq[tmpID] = {
+                ["title"] = dasatz["kat"],
+                ["rules"] = {},
+            }
+            katCounter = katCounter + 1
+        end
+
+        tmpTable = {
+            ["text"] = dasatz["Frage"],
+            ["answer"] = dasatz["Antwort"]
+        }
+
+        table.insert(faq[tmpID]["rules"], tmpTable)
+    end
 end
-addEventHandler ( "onResourceStart",getResourceRootElement(getThisResource()), loadFAQ )
+addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), loadFAQ)
 
-
-addEvent("sendMeFAQ",true)
+addEvent("sendMeFAQ", true)
 function sendMeFAQ_func()
-	triggerClientEvent(source,"addFAQ",source,faq)
+    triggerClientEvent(source, "addFAQ", source, faq)
 end
-addEventHandler("sendMeFAQ",getRootElement(),sendMeFAQ_func)
-
-
-
-
-
-
-
-
+addEventHandler("sendMeFAQ", getRootElement(), sendMeFAQ_func)

19ef28ccca1e199c8bbeedbaef8e806aef38c238
Colin Weber <colin.weber@sap.com>
2016-06-17 14:36:00 +0200

    More Debug

--- terratex_reallife/FIXES/int_dim_server.lua
+++ terratex_reallife/FIXES/int_dim_server.lua
@@ -1,41 +1,33 @@
-addEvent("onIntDimChange",true)
-_setElementInterior=setElementInterior
-function setElementInterior(Element,Interior)
-	if(isElement(Element)) and Interior then
-		triggerEvent("onIntDimChange",Element,Interior,-1)
-		_setElementInterior(Element,Interior)
-	end
-
+addEvent("onIntDimChange", true)
+_setElementInterior = setElementInterior
+function setElementInterior(Element, Interior)
+    if (isElement(Element)) and Interior then
+        triggerEvent("onIntDimChange", Element, Interior, -1)
+        _setElementInterior(Element, Interior)
+    end
 end
 
-_setElementDimension=setElementDimension
-function setElementDimension(Element,dim)
-	if(isElement(Element))and (dim) then
-		triggerEvent("onIntDimChange",Element,-1,dim)
-		_setElementDimension(Element,dim)
-	end
+_setElementDimension = setElementDimension
+function setElementDimension(Element, dim)
+    if (dim < 0) then outputDebugString(debug.traceback()) end
+    assert(dim > -1, "Expected positve number got negative");
+    if (isElement(Element)) and (dim) then
+        triggerEvent("onIntDimChange", Element, -1, dim)
+        _setElementDimension(Element, dim)
+    end
 end
 
+addEvent("clientSetElementDimension", true)
+addEvent("clientSetElementInterior", true)
 
-
-addEvent("clientSetElementDimension",true)
-addEvent("clientSetElementInterior",true)
-
-function clientSetElementDimension_func(thePlayer,dim)
-    setElementDimension(thePlayer,dim)
+function clientSetElementDimension_func(thePlayer, dim)
+    setElementDimension(thePlayer, dim)
 end
 
-function clientSetElementInterior_func(thePlayer,int,x,y,z)
-    setElementInterior(thePlayer,int,x,y,z)
+function clientSetElementInterior_func(thePlayer, int, x, y, z)
+    setElementInterior(thePlayer, int, x, y, z)
 end
 
-addEventHandler("clientSetElementDimension",getRootElement(),clientSetElementDimension_func)
-addEventHandler("clientSetElementInterior",getRootElement(),clientSetElementInterior_func)
-
-
-
-
-
-
-
+addEventHandler("clientSetElementDimension", getRootElement(), clientSetElementDimension_func)
+addEventHandler("clientSetElementInterior", getRootElement(), clientSetElementInterior_func)
 

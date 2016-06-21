d8745685c86a38d06a42eef1fb224f49a33315c2
geramy <admin@terratex.eu>
2016-06-21 22:02:54 +0200

    #31 add Gold is now buyable

--- terratex_reallife/SYSTEM/goldsystem/gold_client.lua
+++ terratex_reallife/SYSTEM/goldsystem/gold_client.lua
@@ -6,6 +6,7 @@
 -- To change this template use File | Settings | File Templates.
 --
 local goldWindow;
+local goldBrowser;
 function startGoldUI()
     if (goldWindow) then
         if isElement(goldWindow) then destroyElement(goldWindow) end
@@ -16,13 +17,20 @@ function startGoldUI()
 
         local browser = guiCreateBrowser(0, 0, 700, 550, true, false, false, goldWindow)
 
-        setBrowserAjaxHandler ( guiGetBrowser(browser), "ajax_gold_buy.html", buyNewGold)
 
         addEventHandler("onClientBrowserCreated", guiGetBrowser(browser),
             function()
-                toggleBrowserDevTools ( guiGetBrowser(browser), true )
+                setBrowserAjaxHandler ( source, "ajax_gold_buy.html", buyNewGold)
+                setDevelopmentMode ( true, true )
+                toggleBrowserDevTools ( source, true )
                 loadBrowserURL(source, "http://mta/local/UI/Gold.html");
-                browserActualizeFields(source);
+            end
+        )
+
+        addEventHandler ( "onClientBrowserDocumentReady" , guiGetBrowser(browser) ,
+            function ( url )
+                browserActualizeFields(source)
+                goldBrowser = source;
             end
         )
     end
@@ -30,17 +38,32 @@ end
 addCommandHandler("gold", startGoldUI, false, false)
 
 function buyNewGold(get, post)
-    outputChatBox(tostring(get.amount))
-    --[[if (get.amount) then
-        if (get.amount * config["gold.price"] <= getPlayerMoney(getLocalPlayer())) then
-
-        else
-            showError(getLocalPlayer(), "Du hast nicht genug Geld um so viel Gold zu kaufen!");
+    if (get) then
+        if (get.amount) then
+            if (get.amount * config["gold.price"] <= getPlayerMoney(getLocalPlayer())) then
+                if (get.amount > 0) then
+                    triggerServerEvent("buyNewGold", getLocalPlayer(), get.amount);
+                end
+            else
+                showError(getLocalPlayer(), "Du hast nicht genug Geld um so viel Gold zu kaufen!");
+            end
         end
-    end]]
+    end
 end
 
 function browserActualizeFields(browser)
-    executeBrowserJavascript ( browser, "$('#imBesitz').html(" .. getElementData(getLocalPlayer(), "Gold") .. ");");
-    executeBrowserJavascript ( browser, "$('#pricePerGold').val(" .. config["gold.price"] .. ");");
+    local js = "$(document).ready(function(){";
+    js = js .. "$('#imBesitz').html(" .. getElementData(getLocalPlayer(), "Gold") .. ");";
+    js = js .. "$('#pricePerGold').val(" .. config["gold.price"] .. "); });";
+
+    executeBrowserJavascript(browser, js);
 end
+
+
+function actualizeGoldAmount_func()
+    if (goldBrowser) then
+        browserActualizeFields(goldBrowser)
+    end
+end
+addEvent("actualizeGoldAmount", true)
+addEventHandler("actualizeGoldAmount", getRootElement(), actualizeGoldAmount_func)
---
+++ terratex_reallife/SYSTEM/goldsystem/gold_server.lua
@@ -0,0 +1,24 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: geramy
+-- Date: 21.06.2016
+-- Time: 21:57
+-- To change this template use File | Settings | File Templates.
+--
+
+function buyNewGold_func(amount)
+    if (amount * config["gold.price"] <= getPlayerMoney(source)) then
+        if (amount > 0) then
+            changePlayerMoney(source, -amount * config["gold.price"], "sonstiges", "Goldkauf");
+
+            vioSetElementData(source, "Gold", vioGetElementData(source, "Gold") + amount);
+            showError(source, "Du hast erfolgreich " .. amount .. "Gold gekauft!");
+
+            triggerClientEvent(source, "actualizeGoldAmount", source)
+        end
+    else
+        showError(source, "Du hast nicht genug Geld um so viel Gold zu kaufen!");
+    end
+end
+addEvent("buyNewGold", true)
+addEventHandler("buyNewGold", getRootElement(), buyNewGold_func)

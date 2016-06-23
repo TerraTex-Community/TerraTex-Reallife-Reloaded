3081d16b5335078041b315b8a1c6e41d86490518
Colin Weber <colin.weber@sap.com>
2016-06-23 14:37:36 +0200

    Removed DB Updater Option because i will not do that... there are more important things and to copy and past some sqls are not soooo hard to do

--- terratex_reallife/database_config_example.lua
+++ terratex_reallife/database_config_example.lua
@@ -11,10 +11,3 @@ config["mysqlhost"]="localhost"
 config["mysqluser"]=""
 config["mysqlpassword"]=""
 config["mysqldb"]=""
-
--- This Optional enables the database Update and Install system
--- You should only used it, if you want a clean new install or you want to use all updates from git without own changes
--- It is highly recommend to disable this option if you change any code that is related to the database or if you change things in the database structure
--- In this case: Import & Modify SQL Files manualy
--- @info Not Implemented Yet!
-config["database_updater"] = true;

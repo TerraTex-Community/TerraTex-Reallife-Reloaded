58fa6d5986459fc75509937e44dd0667cce9af12
scriptcodeDE <broehlfelix@googlemail.com>
2016-08-28 17:19:53 +0200

    Keine ineinander spawnenden Mechaniker-Autos

--- terratex_reallife/ENVIRO/jobs/mechaniker/mechaniker_vehicles.lua
+++ terratex_reallife/ENVIRO/jobs/mechaniker/mechaniker_vehicles.lua
@@ -7,8 +7,7 @@ function createMechVehicles()
 	mechVehicles[1]= createVehicle(552, 1651.1211,-1818.4062,13.2662,0,0,180)    
 	mechVehicles[2]= createVehicle(552,1651.121,-1828.479,13.2662,0,0,180)
 	mechVehicles[3]= createVehicle(552, 1651.1211,-1808.333,13.2662,0,0,180)
-	mechVehicles[4]= createVehicle(552,1651.1211,-1828.4785,13.2662,0,0,180)
-	mechVehicles[5]= createVehicle(552,1635.234,-1844.325,13.2662,0,0,0)
+	mechVehicles[4]= createVehicle(552,1635.234,-1844.325,13.2662,0,0,0)
 	mechVehicles[5]= createVehicle(552,1630.2676,-1844.3252,13.2662,0,0,0)
 
 

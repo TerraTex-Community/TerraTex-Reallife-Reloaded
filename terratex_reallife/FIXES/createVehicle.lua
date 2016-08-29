69557e3c326fc447879537e615d978a38e0a9d0d
Geramy92 <gamergeramy@googlemail.com>
2016-08-29 17:37:46 +0200

    test

--- terratex_reallife/FIXES/createVehicle.lua
+++ terratex_reallife/FIXES/createVehicle.lua
@@ -6,27 +6,27 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-local _createVehicle = createVehicle
-
-function createVehicle(model, x, y, z, rx, ry, rz, numberplate, bDirection, variant1, variant2)
-    local vehicle = _createVehicle(model, x, y, z, rx, ry, rz, numberplate, bDirection, variant1, variant2);
-
-    local spawnX, spawnY, spawnZ, spawnRX, spawnRY, spawnRZ = 0, 0, 0, 0, 0, 0;
-    if x and y and z then
-        spawnX = x;
-        spawnY = y;
-        spawnZ = z;
-        if (rx) then
-            spawnRX = rx;
-        end
-        if (ry) then
-            spawnRY = ry;
-        end
-        if (rz) then
-            spawnRZ = rz;
-        end
-    end
-
-    vioSetElementData(vehicle, "_getVehicleRespawn", {spawnX, spawnY, spawnZ, spawnRX, spawnRY, spawnRZ});
-end
+--local _createVehicle = createVehicle
+--
+--function createVehicle(model, x, y, z, rx, ry, rz, numberplate, bDirection, variant1, variant2)
+--    local vehicle = _createVehicle(model, x, y, z, rx, ry, rz, numberplate, bDirection, variant1, variant2);
+--
+--    local spawnX, spawnY, spawnZ, spawnRX, spawnRY, spawnRZ = 0, 0, 0, 0, 0, 0;
+--    if x and y and z then
+--        spawnX = x;
+--        spawnY = y;
+--        spawnZ = z;
+--        if (rx) then
+--            spawnRX = rx;
+--        end
+--        if (ry) then
+--            spawnRY = ry;
+--        end
+--        if (rz) then
+--            spawnRZ = rz;
+--        end
+--    end
+--
+--    vioSetElementData(vehicle, "_getVehicleRespawn", {spawnX, spawnY, spawnZ, spawnRX, spawnRY, spawnRZ});
+--end
 

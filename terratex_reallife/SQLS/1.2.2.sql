913de77b54e4b6321b505f474539052dd49a994d
geramy <admin@terratex.eu>
2016-09-03 18:38:58 +0200

    reorder sql

--- terratex_reallife/SQLS/1.2.2.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `objects_businesses` (`ID`, `Name`, `Preis`, `Kasse`, `x`, `y`, `z`) VALUES (NULL, 'Tombupot Lotterie', '150000', '0', '1628.5', '-1171.5', '24.1');
--- terratex_reallife/SQLS/update.1.2.1.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `objects_teleport_markers` SET `x` = '1317.09' WHERE `objects_teleport_markers`.`ID` = 40;

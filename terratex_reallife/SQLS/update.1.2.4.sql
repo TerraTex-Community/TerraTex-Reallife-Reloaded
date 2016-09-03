7c9e86253d195c51ddda17d5de4e2a7f607d46dc
scriptcodeDE <broehlfelix@googlemail.com>
2016-09-03 18:02:26 +0200

    Update update.1.2.4.sql

--- terratex_reallife/SQLS/update.1.2.4.sql
+++ terratex_reallife/SQLS/update.1.2.4.sql
@@ -1,3 +1,2 @@
-ALTER TABLE `objects_drivein` ADD `size` FLOAT NOT NULL AFTER `z`;
-UPDATE `objects_drivein` SET `size`=4;
+ALTER TABLE `objects_drivein` ADD `size` FLOAT NOT NULL DEFAULT '4' AFTER `z`;
 INSERT INTO `objects_drivein` (`ID`, `x`, `y`, `z`, `size`, `typ`) VALUES (NULL, '1766.60546875', '-1892.126953125', '13', '1', 'pizza');

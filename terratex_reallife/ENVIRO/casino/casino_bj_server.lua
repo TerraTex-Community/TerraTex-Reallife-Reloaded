6781a8e7ae98e24d5bf02420fbd3bc210ab22a6f
Geramy92 <gamergeramy@googlemail.com>
2016-09-06 15:31:51 +0200

    fix for #283

--- terratex_reallife/ENVIRO/casino/casino_bj_server.lua
+++ terratex_reallife/ENVIRO/casino/casino_bj_server.lua
@@ -3,7 +3,7 @@ local saveTagesGewinnePerSpieler = {} --index = spieler , tabelle: {["pferde"]=g
 addEvent("reduceBlackJackHandMoney", true)
 function reduceBlackJackHandMoney_func(money, typ)
     changePlayerMoney(source, -money, "sonstiges", "Casino", "Wetteinsatz")
-    changeBizKasse(13, money, "Casino Wetteinsatz")
+    changeBizKasse(13, money/10, "Casino Wetteinsatz")
     outputChatBox(string.format("Dein Einsatz wurde um %s erhöht!", toprice(money)), source, 0, 255, 0)
     --outputChatBox("Aufgrund von Bugusing wurde der Einsatz nur virtuell erhöht ohne vom Handgeld abgezogen zu werden!",source,255,0,0)
     if not saveTagesGewinnePerSpieler[getPlayerName(source)] then saveTagesGewinnePerSpieler[getPlayerName(source)] = {} end
@@ -24,7 +24,7 @@ addEventHandler("reduceBlackJackHandMoney", getRootElement(), reduceBlackJackHan
 addEvent("payOut_Bj", true)
 function payOut_Bj_func(money, typ)
     changePlayerMoney(source, money, "sonstiges", "Casino", "Auszahlung")
-    changeBizKasse(13, money, "Casino Auszahlung")
+    changeBizKasse(13, -money/10, "Casino Auszahlung")
     outputChatBox(string.format("Dein Gewinn %s wurde ausgezahlt!", toprice(money)), source, 0, 255, 0)
     --outputChatBox("Aufgrund von Bugusing wurde der Gewinn nur virtuell ausgezahlt ohne auf das Handgeld ausgezahlt zu werden!",source,255,0,0)
     if not saveTagesGewinnePerSpieler[getPlayerName(source)] then saveTagesGewinnePerSpieler[getPlayerName(source)] = {} end
@@ -38,13 +38,5 @@ function payOut_Bj_func(money, typ)
         tagesgewinn = saveTagesGewinnePerSpieler[getPlayerName(source)][typ]
     });
 end
-
 addEventHandler("payOut_Bj", getRootElement(), payOut_Bj_func)
-
-
-
-
-
-
-
 

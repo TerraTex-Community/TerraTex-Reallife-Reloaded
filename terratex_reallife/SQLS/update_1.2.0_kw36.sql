7a2a83dd780f08ad7a81c3978c48baa3b621a0f5
Geramy92 <gamergeramy@googlemail.com>
2016-09-06 15:22:58 +0200

    correct sql files

--- terratex_reallife/SQLS/update_1.2.0_kw36.sql
+++ terratex_reallife/SQLS/update_1.2.0_kw36.sql
@@ -1,3 +1,7 @@
+SET FOREIGN_KEY_CHECKS=0;
+SET GLOBAL FOREIGN_KEY_CHECKS=0;
 INSERT INTO `objects_teleport_markers` (`ID`, `x`, `y`, `z`, `interior`, `dimension`, `markerType`, `markerSize`, `toMarker`, `toPosX`, `toPosY`, `toPosZ`, `rzAfterTeleport`, `toInt`, `toDim`, `specialKey`, `description`) VALUES
 (59, 1788.37, -1298.52, 12.7, 0, 0, 'corona', 2.0, 60, NULL, NULL, NULL, NULL, 0, 0, NULL, 'Hochhaus neben Glenpark IN'),
 (60, 1824.51, -1300.54, 119.734, 0, 0, 'corona', 2.0, 59, NULL, NULL, NULL, NULL, 0, 0, NULL, 'Hochhaus neben Glenpark OUT');
+SET FOREIGN_KEY_CHECKS=1;
+SET GLOBAL FOREIGN_KEY_CHECKS=1;

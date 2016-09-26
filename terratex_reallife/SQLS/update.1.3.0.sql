ce0181de878d7f5bd26f93e064e654d0cf8d2653
Geramy92 <gamergeramy@googlemail.com>
2016-09-26 14:32:31 +0200

    #353 add new log

--- terratex_reallife/SQLS/update.1.3.0.sql
+++ terratex_reallife/SQLS/update.1.3.0.sql
@@ -1,1 +1,11 @@
 ALTER TABLE user_data ADD afktime INT DEFAULT 0 NULL;
+
+CREATE TABLE `log_playtime` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Nickname` varchar(255) DEFAULT NULL,
+  `login` timestamp NOT NULL,
+  `logout` timestamp NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `log_playtime_user_Nickname_fk` (`Nickname`),
+  CONSTRAINT `log_playtime_user_Nickname_fk` FOREIGN KEY (`Nickname`) REFERENCES `user` (`Nickname`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;

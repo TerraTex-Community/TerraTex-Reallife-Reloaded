74c984da18db298676dafbb5670828be69c4e246
Geramy92 <gamergeramy@googlemail.com>
2016-10-17 16:27:05 +0200

    #371 implement spawning

--- terratex_reallife/ENVIRO/fraktionen/gangfight/run_gf.lua
+++ terratex_reallife/ENVIRO/fraktionen/gangfight/run_gf.lua
@@ -5,17 +5,6 @@
 -- Time: 15:17
 -- To change this template use File | Settings | File Templates.
 --
--- DB:
---Name = Name,
---Type = Type,
---X = X,
---Y = Y,
---Z = Z,
---ASpawnX = ASpawnX,
---ASpawnY = ASpawnY,
---ASpawnZ = ASpawnZ,
---RSmall = RSmall,
---RLarge = RLarge
 
 local colshapes = {};
 
@@ -45,6 +34,8 @@ function startGf()
     table.insert(colshapes, killPlayerColShape);
     setElementDimension(killPlayerColShape, 1337);
     addEventHandler("onColShapeLeave", killPlayerColShape, sendKillWarning);
+
+    startRound();
 end
 
 function sendKillWarning(element, matchDim)
@@ -65,4 +56,78 @@ function destroyAllColShapes()
         destroyElement(theColShape);
     end
     colshapes = {};
+end
+
+-- GF
+function startRound()
+--    Spawn Players Team Attack
+    local gfElement = getElementById("GFSync");
+    local data = vioGetElementData(gfElement, "data");
+    local gfPosition = data.attack;
+    local gfPositionData = vioGetElementData(gfPosition, "data");
+
+    for theKey, thePlayer in ipairs(data.attackers) do
+        killPed(thePlayer);
+    end
+    for theKey, thePlayer in ipairs(data.defenders) do
+        killPed(thePlayer);
+    end
+
+    data.round = data.round + 1;
+    if (data.round % 2 == 1) then
+        -- in rounds 1,3 and 5 spawn Attacker Team
+        for theKey, thePlayer in ipairs(data.attackers) do
+            if (isElement(thePlayer)) then
+                spawnPlayer ( thePlayer, gfPositionData.X, gfPositionData.Y, gfPositionData.Z, 0, getElementModel(thePlayer), 0, 1337);
+                setElementCollisionsEnabled(thePlayer, false);
+                outputChatBox("Und die nächste Runde beginnt... bereitet euch vor und verteidigt den Laden! Das Gegnerteam spawned in 5 Sekunden.", thePlayer);
+                givePlayerGFWeapons(thePlayer);
+            end
+        end
+    else
+        -- in rounds 2,4 spawn Defender Team
+        for theKey, thePlayer in ipairs(data.defenders) do
+            if (isElement(thePlayer)) then
+                spawnPlayer ( thePlayer, gfPositionData.X, gfPositionData.Y, gfPositionData.Z, 0, getElementModel(thePlayer), 0, 1337);
+                setElementCollisionsEnabled(thePlayer, false);
+                outputChatBox("Und die nächste Runde beginnt... bereitet euch vor und verteidigt den Laden! Das Gegnerteam spawned in 5 Sekunden.", thePlayer);
+                givePlayerGFWeapons(thePlayer);
+            end
+        end
+    end
+
+    setTimer(spawnOtherGFTeam, 5000, 1);
+end
+
+function spawnOtherGFTeam()
+    local gfElement = getElementById("GFSync");
+    local data = vioGetElementData(gfElement, "data");
+    local gfPosition = data.attack;
+    local gfPositionData = vioGetElementData(gfPosition, "data");
+
+    if (data.round % 2 == 1) then
+        -- in rounds 1,3 and 5 spawn Attacker Team
+        for theKey, thePlayer in ipairs(data.defenders) do
+            if (isElement(thePlayer)) then
+                spawnPlayer ( thePlayer, gfPositionData.ASpawnX, gfPositionData.ASpawnY, gfPositionData.ASpawnZ, 0, getElementModel(thePlayer), 0, 1337);
+                setElementCollisionsEnabled(thePlayer, false);
+                outputChatBox("Und die nächste Runde beginnt... Erobert den Laden!", thePlayer);
+                givePlayerGFWeapons(thePlayer);
+            end
+        end
+    else
+        -- in rounds 2,4 spawn Defender Team
+        for theKey, thePlayer in ipairs(data.attackers) do
+            if (isElement(thePlayer)) then
+                spawnPlayer ( thePlayer, gfPositionData.ASpawnX, gfPositionData.ASpawnY, gfPositionData.ASpawnZ, 0, getElementModel(thePlayer), 0, 1337);
+                setElementCollisionsEnabled(thePlayer, false);
+                outputChatBox("Und die nächste Runde beginnt... Erobert den Laden!", thePlayer);
+                givePlayerGFWeapons(thePlayer);
+            end
+        end
+    end
+end
+
+function givePlayerGFWeapons(thePlayer)
+--    @todo: implement
 end

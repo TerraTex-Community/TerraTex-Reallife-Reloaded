f886912526baf6430b6ff7d9856afb12fd624d47
geramy <admin@terratex.eu>
2016-10-20 19:42:00 +0200

    #371 add Counter

--- terratex_reallife/ENVIRO/fraktionen/gangfight/run_gf_client.lua
+++ terratex_reallife/ENVIRO/fraktionen/gangfight/run_gf_client.lua
@@ -37,5 +37,29 @@ function showKillGfText()
         local screenW, screenH = guiGetScreenSize();
         dxDrawText ( text, 0,0, screenW, screenH, tocolor(255,0,0), 1.5, "default", "center", "center", false, true, true, false, false, 0, 0, 0 );
     end
+
+    local gfElement = getElementById("GFSync");
+    local data = getElementData(gfElement, "data");
+
+    if (isTimer(data.timer) and (table.hasValue(data.Attackers, getLocalPlayer()) or table.hasValue(data.Defenders, getLocalPlayer()))) then
+--        Spieler ist im GF
+        local restTime = math.floor(getTimerDetails ( data.timer ) / 1000);
+        local minutes = math.floor(restTime / 60);
+        local seconds = math.floor(restTime - (minutes * 60));
+        if (seconds < 10) then
+            seconds = "0" .. seconds;
+        end
+
+        local screenX, screenY = getScreenSize();
+
+        local sX = (screenX / 2) - (456 / 2)
+        local sY = (screenY / 2) - (56 / 2)
+
+        dxDrawRectangle(sX,sY,456.0,56.0,tocolor(0,0,0,255),false)
+        local taxastring="Rundenzeit: "..minutes..":"..seconds;
+
+        dxDrawText(taxastring,sX,sY,(sX + 456),(sY + 56), tocolor(255,255,255,255),2.0,"default","center","center",false,false,false)
+
+    end
 end
 addEventHandler ( "onClientRender", getRootElement(), showKillGfText )

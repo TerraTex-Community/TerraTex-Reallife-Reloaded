85903884ab41aa90953e1dc6af8ceaab11580013
Colin Weber <colin.weber@sap.com>
2016-11-16 17:23:16 +0100

    weather notice

--- terratex_reallife/SYSTEM/wettersys/wetter.lua
+++ terratex_reallife/SYSTEM/wettersys/wetter.lua
@@ -391,3 +391,20 @@ function sendWetterEventToClient()
     setTimer(sendWetterEventToClient, 60000, 1)
 end
 addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), sendWetterEventToClient)
+--http://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today&formatted=0
+--function TimeStamp(dateStringArg)
+--
+--    local inYear, inMonth, inDay, inHour, inMinute, inSecond, inZone =
+--    string.match(dateStringArg, '^(%d%d%d%d)-(%d%d)-(%d%d)T(%d%d):(%d%d):(%d%d)(.-)$')
+--
+--    local zHours, zMinutes = string.match(inZone, '^(.-):(%d%d)$')
+--
+--    local returnTime = os.time({year=inYear, month=inMonth, day=inDay, hour=inHour, min=inMinute, sec=inSecond, isdst=false})
+--
+--    if zHours then
+--        returnTime = returnTime - ((tonumber(zHours)*3600) + (tonumber(zMinutes)*60))
+--    end
+--
+--    return returnTime
+--
+--end

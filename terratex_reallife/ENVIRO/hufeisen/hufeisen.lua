de25eddfce9c4d1a210057fbab08806bfcbc1053
Colin Weber <colin.weber@sap.com>
2016-11-18 13:55:19 +0100

    change hufeisen price

--- terratex_reallife/ENVIRO/hufeisen/hufeisen.lua
+++ terratex_reallife/ENVIRO/hufeisen/hufeisen.lua
@@ -1,32 +1,30 @@
-addEvent("foundHufeisen_func",true)
+addEvent("foundHufeisen_func", true)
 function foundHufeisen()
-	vioSetElementData(source,"Hufeisen",vioGetElementData(source,"Hufeisen") + 1)
-	outputChatBox("Du hast ein Hufeisen gefunden! Es war 1000$ wert!",source,100,255,100)
-    changePlayerMoney(source,1000,"sonstiges","Hufeisen")
-	
-	if(vioGetElementData(source,"Hufeisen")>9)then
-		if(vioGetElementData(source,"Erfolg_10Hufeisen")==0)then
-			vioSetElementData(source,"Erfolg_10Hufeisen",1)
-			triggerClientEvent(source,"onClientCreatePokalGUI",source,"Hufeisenprinz","Finde 10 Hufeisen")
-		
-		end	
-	end
+    vioSetElementData(source, "Hufeisen", vioGetElementData(source, "Hufeisen") + 1)
+    outputChatBox("Du hast ein Hufeisen gefunden! Es war 1000$ wert!", source, 100, 255, 100)
+    changePlayerMoney(source, 1000, "sonstiges", "Hufeisen")
 
-	if(vioGetElementData(source,"Hufeisen")>99)then
-		if(vioGetElementData(source,"Erfolg_100Hufeisen")==0)then
-			vioSetElementData(source,"Erfolg_100Hufeisen",1)
-			triggerClientEvent(source,"onClientCreatePokalGUI",source,"Hufeisenkönig","Finde 100 Hufeisen")
-		
-		end	
-	end
+    if (vioGetElementData(source, "Hufeisen") > 9) then
+        if (vioGetElementData(source, "Erfolg_10Hufeisen") == 0) then
+            vioSetElementData(source, "Erfolg_10Hufeisen", 1)
+            triggerClientEvent(source, "onClientCreatePokalGUI", source, "Hufeisenprinz", "Finde 10 Hufeisen")
+        end
+    end
 
-	if(vioGetElementData(source,"Hufeisen")>999)then
-		if(vioGetElementData(source,"Erfolg_1000Hufeisen")==0)then
-			vioSetElementData(source,"Erfolg_1000Hufeisen",1)
-			triggerClientEvent(source,"onClientCreatePokalGUI",source,"Hufeisenkaiser","Finde 1000 Hufeisen")
-		
-		end	
-	end
+    if (vioGetElementData(source, "Hufeisen") > 99) then
+        if (vioGetElementData(source, "Erfolg_100Hufeisen") == 0) then
+            vioSetElementData(source, "Erfolg_100Hufeisen", 1)
+            triggerClientEvent(source, "onClientCreatePokalGUI", source, "Hufeisenkönig", "Finde 100 Hufeisen")
+        end
+    end
+
+    if (vioGetElementData(source, "Hufeisen") > 999) then
+        if (vioGetElementData(source, "Erfolg_1000Hufeisen") == 0) then
+            vioSetElementData(source, "Erfolg_1000Hufeisen", 1)
+            triggerClientEvent(source, "onClientCreatePokalGUI", source, "Hufeisenkaiser", "Finde 1000 Hufeisen")
+        end
+    end
 end
-addEventHandler("foundHufeisen_func",getRootElement(),foundHufeisen)
+
+addEventHandler("foundHufeisen_func", getRootElement(), foundHufeisen)
 

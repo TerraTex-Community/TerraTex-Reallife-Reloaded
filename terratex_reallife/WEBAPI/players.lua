02c7981000d5a56fa20b6c83e0bc3bd94d8ccfcf
Colin Weber <colin.weber@sap.com>
2016-11-24 14:32:34 +0100

    add API configuration

---
+++ terratex_reallife/WEBAPI/players.lua
@@ -0,0 +1,13 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: C5217649
+-- Date: 24.11.2016
+-- Time: 14:29
+-- To change this template use File | Settings | File Templates.
+--
+
+function apiGetListOfPlayers()
+    local players = getElementsByType("player");
+    return players;
+end
+

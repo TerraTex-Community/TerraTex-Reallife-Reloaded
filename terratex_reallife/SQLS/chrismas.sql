6421710ae852dffeab9efd494e3f96217ffc0642
Geramy <geramy92@users.noreply.github.com>
2017-09-02 20:37:35 +0200

    Delete chrismas.sql

--- terratex_reallife/SQLS/chrismas.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `data_chrismas` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Nickname` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `data_chrismas_user_Nickname_fk` (`Nickname`),
-  CONSTRAINT `data_chrismas_user_Nickname_fk` FOREIGN KEY (`Nickname`) REFERENCES `user` (`Nickname`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;

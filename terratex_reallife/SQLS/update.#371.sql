83aff3350822b1e3262343bddb8535b153d1e8e1
Geramy <geramy92@users.noreply.github.com>
2017-09-02 20:26:18 +0200

    Update update.#371.sql

--- terratex_reallife/SQLS/update.#371.sql
+++ terratex_reallife/SQLS/update.#371.sql
@@ -1,5 +1,3 @@
-DROP TABLE faction_shops;
-
 CREATE TABLE `faction_gangfights` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) DEFAULT NULL,
@@ -25,5 +23,3 @@ INSERT INTO faction_gangfights (Name, Type, Owner, X, Y, Z, RSmall, RLarge, ASpa
 INSERT INTO faction_gangfights (Name, Type, Owner, X, Y, Z, RSmall, RLarge, ASpawnX, ASpawnY, ASpawnZ, HoursWithoutAttack) VALUES ('Hauptammunation LV', 2, -1, 2538.39, 2082.63, 10.8203, 112, 265, 2572.88, 2258.19, 10.8133, 0);
 INSERT INTO faction_gangfights (Name, Type, Owner, X, Y, Z, RSmall, RLarge, ASpawnX, ASpawnY, ASpawnZ, HoursWithoutAttack) VALUES ('Drogenversteck Süd LV', 1, -1, 2425.37, 1899.14, 6.01562, 198, 456, 2596.58, 1726.8, 10.3833, 0);
 INSERT INTO faction_gangfights (Name, Type, Owner, X, Y, Z, RSmall, RLarge, ASpawnX, ASpawnY, ASpawnZ, HoursWithoutAttack) VALUES ('Drogenversteck Nord LV', 1, -1, 2647.27, 828.799, 8.82031, 135, 398, 2632.43, 1079.42, 10.8203, 0);
-
-DROP TABLE faction_shops;

3d5f01dc0e6185b17f9912289838d6b1e5a28141
geramy92 <info@geramy92.de>
2020-01-18 21:17:52 +0100

    #442 fix math.random

--- terratex_reallife/USEFULL_SCRIPTS/number_math_functions.lua
+++ terratex_reallife/USEFULL_SCRIPTS/number_math_functions.lua
@@ -20,8 +20,11 @@ function math.round(number, decimals, method)
 
     decimals = decimals or 0
     local factor = 10 ^ decimals
-    if (method == "ceil" or method == "floor") then return math[method](number * factor) / factor
-    else return tonumber(("%."..decimals.."f"):format(number)) end
+    if (method == "ceil" or method == "floor") then
+        return math[method](number * factor) / factor
+    else
+        return tonumber(("%." .. decimals .. "f"):format(number))
+    end
 end
 
 function toprice(number)
@@ -31,46 +34,50 @@ function toprice(number)
 end
 
 function math.Tausend(v)
-    local isnegetive=false
-    if(v<0)then
-        isnegetive=true
-        v=-v
+    local isnegetive = false
+    if (v < 0) then
+        isnegetive = true
+        v = -v
     end
     local s = string.format("%d", math.floor(v))
     local pos = string.len(s) % 3
-    if pos == 0 then pos = 3 end
-    if(not(isnegetive))then
+    if pos == 0 then
+        pos = 3
+    end
+    if (not (isnegetive)) then
         return string.sub(s, 1, pos)
-                .. string.gsub(string.sub(s, pos+1), "(...)", ",%1")
+                .. string.gsub(string.sub(s, pos + 1), "(...)", ",%1")
                 .. string.sub(string.format("%.2f", v - math.floor(v)), 2)
     else
-        return "-"..string.sub(s, 1, pos)
-                .. string.gsub(string.sub(s, pos+1), "(...)", ",%1")
+        return "-" .. string.sub(s, 1, pos)
+                .. string.gsub(string.sub(s, pos + 1), "(...)", ",%1")
                 .. string.sub(string.format("%.2f", v - math.floor(v)), 2)
     end
 
 end
 
+math.__random = math.random
+function math.random(value, maxvalue)
 
-math.__random=math.random
-function math.random(value,maxvalue)
     if (not value) then
         return math.__random()
     end
 
-    if(not maxvalue)then
+    if (not maxvalue) then
         return math.__random(value)
     end
-    
-    
-    if(value==maxvalue)then
+
+    if (value == maxvalue) then
         return value
     else
-        return math.__random(value,maxvalue)
+        return math.__random(value, maxvalue)
     end
 end
 
-
+function initMathRandom()
+    math.randomseed(os.time())
+end
+addEventHandler("onResourceStart", getRootElement(), initMathRandom)
 
 
 

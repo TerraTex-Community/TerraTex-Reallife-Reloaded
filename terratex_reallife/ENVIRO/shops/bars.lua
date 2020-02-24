8ab85d5fcc1c05debf812ab040f738340fc2902b
geramy92 <info@geramy92.de>
2020-02-24 20:59:54 +0100

    fix variable names

--- terratex_reallife/ENVIRO/shops/bars.lua
+++ terratex_reallife/ENVIRO/shops/bars.lua
@@ -1,46 +1,46 @@
-local barbot = {}
-local stripperbot = {}
+local barBot = {}
+local stripperBot = {}
 local stripperSkinID = {63,87,92,138,140,139,152,178,237,238,246,244,245,256}
 
-function barbots()
+function barBots()
 	respawnBarBots()
     respawnStripperBots()
 	
 	setPedDancingQueen()
 end
-addEventHandler("onResourceStart",getResourceRootElement(getThisResource()),barbots)
+addEventHandler("onResourceStart",getResourceRootElement(getThisResource()),barBots)
 
 function respawnBarBots()
-	for theKey, theBot in pairs(barbot) do
+	for theKey, theBot in pairs(barBot) do
         if (isElement(theBot)) then
             destroyElement(theBot)
         end
     end
     
 	--Bar Frauen
-	barbot[1]=createPed(11,501.8388671875,-19.0673828125,1000.671875,89.916229248047)--alhambra
-	setElementInterior(barbot[1],17)
-	addEventHandler("onPedWasted",barbot[1],respawnBarBots)
+	barBot[1]=createPed(11,501.8388671875,-19.0673828125,1000.671875,89.916229248047)--alhambra
+	setElementInterior(barBot[1],17)
+	addEventHandler("onPedWasted",barBot[1],respawnBarBots)
 	
-	barbot[2]=createPed(11,499.775390625,-77.802734375,998.7578125,354.44357299805)--green bottels
-	setElementInterior(barbot[2],11)
-	addEventHandler("onPedWasted",barbot[2],respawnBarBots)
+	barBot[2]=createPed(11,499.775390625,-77.802734375,998.7578125,354.44357299805)--green bottels
+	setElementInterior(barBot[2],11)
+	addEventHandler("onPedWasted",barBot[2],respawnBarBots)
 	
-	barbot[3]=createPed(11,1214.1318359375,-15.2646484375,1000.921875,353.83383178711)--gPigPen
-	setElementInterior(barbot[3],2)
-	addEventHandler("onPedWasted",barbot[3],respawnBarBots)
+	barBot[3]=createPed(11,1214.1318359375,-15.2646484375,1000.921875,353.83383178711)--gPigPen
+	setElementInterior(barBot[3],2)
+	addEventHandler("onPedWasted",barBot[3],respawnBarBots)
     
-    barbot[4]=createPed(11,1761.8193359375,-1921.47265625,16.2,0)--Marktplatz am Rookiespawn
-	addEventHandler("onPedWasted",barbot[4],respawnBarBots)
+    barBot[4]=createPed(11,1761.8193359375,-1921.47265625,16.2,0)--Marktplatz am Rookiespawn
+	addEventHandler("onPedWasted",barBot[4],respawnBarBots)
     
-    for theKey, theBot in pairs(stripperbot) do
+    for theKey, theBot in pairs(stripperBot) do
         setElementFrozen(theBot, true)
     end
 end
 
 
 function respawnStripperBots()
-    for theKey, theBot in pairs(stripperbot) do
+    for theKey, theBot in pairs(stripperBot) do
         if (isElement(theBot)) then
             destroyElement(theBot)
         end
@@ -48,38 +48,38 @@ function respawnStripperBots()
 
 	--stripper
 	randid=stripperSkinID[math.random(1,table.getn(stripperSkinID))]
-	stripperbot[1]=createPed(randid,1208.376953125,-6.9892578125,1001.328125,6.6825256347656)--gPigPen stripper 1
-	setElementInterior(stripperbot[1],2)
-	addEventHandler("onPedWasted",stripperbot[1],respawnStripperBots)
+	stripperBot[1]=createPed(randid,1208.376953125,-6.9892578125,1001.328125,6.6825256347656)--gPigPen stripper 1
+	setElementInterior(stripperBot[1],2)
+	addEventHandler("onPedWasted",stripperBot[1],respawnStripperBots)
 	
 	randid=stripperSkinID[math.random(1,table.getn(stripperSkinID))]
-	stripperbot[2]=createPed(randid,1214.048828125,-4.3095703125,1001.328125,50.238494873047)--gPigPen stripper 2
-	setElementInterior(stripperbot[2],2)
-	addEventHandler("onPedWasted",stripperbot[2],respawnStripperBots)
+	stripperBot[2]=createPed(randid,1214.048828125,-4.3095703125,1001.328125,50.238494873047)--gPigPen stripper 2
+	setElementInterior(stripperBot[2],2)
+	addEventHandler("onPedWasted",stripperBot[2],respawnStripperBots)
 	
 	randid=stripperSkinID[math.random(1,table.getn(stripperSkinID))]
-	stripperbot[3]=createPed(randid,1221.134765625,8.7587890625,1001.3356323242,127.63287353516)--gPigPen stripper 3
-	setElementInterior(stripperbot[3],2)
-	addEventHandler("onPedWasted",stripperbot[3],respawnStripperBots)
+	stripperBot[3]=createPed(randid,1221.134765625,8.7587890625,1001.3356323242,127.63287353516)--gPigPen stripper 3
+	setElementInterior(stripperBot[3],2)
+	addEventHandler("onPedWasted",stripperBot[3],respawnStripperBots)
 	
 	randid=stripperSkinID[math.random(1,table.getn(stripperSkinID))]
-	stripperbot[4]=createPed(randid,1223.09765625,-2.9375,1001.328125,23.942321777344)--gPigPen stripper 4
-	setElementInterior(stripperbot[4],2)
-	addEventHandler("onPedWasted",stripperbot[4],respawnStripperBots)
+	stripperBot[4]=createPed(randid,1223.09765625,-2.9375,1001.328125,23.942321777344)--gPigPen stripper 4
+	setElementInterior(stripperBot[4],2)
+	addEventHandler("onPedWasted",stripperBot[4],respawnStripperBots)
 	
 	randid=stripperSkinID[math.random(1,table.getn(stripperSkinID))]
-	stripperbot[5]=createPed(randid,1223.09765625,-9.8310546875,1001.328125,164.00366210938)--gPigPen stripper 5
-	setElementInterior(stripperbot[5],2)
-	addEventHandler("onPedWasted",stripperbot[5],respawnStripperBots)
+	stripperBot[5]=createPed(randid,1223.09765625,-9.8310546875,1001.328125,164.00366210938)--gPigPen stripper 5
+	setElementInterior(stripperBot[5],2)
+	addEventHandler("onPedWasted",stripperBot[5],respawnStripperBots)
     
-    for theKey, theBot in pairs(stripperbot) do
+    for theKey, theBot in pairs(stripperBot) do
         setElementFrozen(theBot, true)
     end
 end
 
 local dances={"STR_Loop_A","STR_Loop_B","STR_Loop_C","strip_A","strip_B","strip_C","strip_D","strip_E","strip_F","strip_G" }
 function setPedDancingQueen()
-    for theKey, theBot in pairs(stripperbot) do
+    for theKey, theBot in pairs(stripperBot) do
         setPedAnimation(theBot, "STRIP", dances[math.random(1, table.getn(dances))], -1, true, true, false, false)
     end
     
@@ -88,7 +88,7 @@ end
 
 function onPlayerClickOnBarTusse(mouseButton, buttonState, clickedElement)
     if (mouseButton == "left" and buttonState == "down") then
-        for theKey, theBot in pairs(barbot) do
+        for theKey, theBot in pairs(barBot) do
             if (clickedElement == theBot) then
                 outputChatBox("Hey Süßer! Kauf dir doch was zu trinken mit /drink", source, 135,237,225)
                 break
@@ -101,7 +101,7 @@ addEventHandler("onPlayerClick", getRootElement(), onPlayerClickOnBarTusse)
 function drink_func(thePlayer, cmd, text)
 	local px, py, pz = getElementPosition(thePlayer)
     local nextTo = false
-    for theKey, theBot in pairs(barbot) do
+    for theKey, theBot in pairs(barBot) do
         local bx, by, bz = getElementPosition(theBot)
         local dis = getDistanceBetweenPoints3D(bx, by, bz, px, py, pz)
         if (dis < 10) then
